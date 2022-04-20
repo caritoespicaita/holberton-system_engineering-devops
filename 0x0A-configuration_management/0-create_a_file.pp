7cc7f9d42a8b721068fabe40bccbe817ae6bba91
caritoespicaita <caritoespicaita@hotmail.com>
2022-04-19 22:33:03 -0500

    fix style

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,9 +2,8 @@
 
 file { 'create a file':
   path    => '/tmp/school',
-  ensure  => 'file',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+  content => 'I love Puppet'
 }
