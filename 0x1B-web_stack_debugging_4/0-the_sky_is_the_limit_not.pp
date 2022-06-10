02eb152c8759d3bdb327afe8618e06bc222f4bff
caritoespicaita <caritoespicaita@hotmail.com>
2022-06-09 23:44:27 -0500

    update task

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+nes (9 sloc)  235 Bytes
+#Increase the amount of nginx traffic
+
+exec { 'for-nginx':
+  command => 'sed -i "s/15/8192/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+-> exec { 'restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
